0aaed712eba7d2adb8d6e06bd24e32d09ab1d06d
SaikalSdd <38737621+SaikalSdd@users.noreply.github.com>
2019-01-23 23:02:07 +0600

    Add files via upload

---
+++ zadanie4ex2.js
@@ -0,0 +1,13 @@
+var x = document.getElementsByClassName("element");
+x[0].style.color = "red";
+var x = document.getElementsByClassName("element");
+x[1].style.color = "red";
+var x = document.getElementsByClassName("element");
+x[2].style.color = "red";
+
+var x = document.getElementsByClassName("element");
+x[3].style.color = "green";
+var x = document.getElementsByClassName("element");
+x[4].style.color = "green";
+var x = document.getElementsByClassName("element");
+x[5].style.color = "green";
