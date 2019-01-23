b607b4423ad4a8e35187ae5cf5c9d332ea8fa8cf
SaikalSdd <38737621+SaikalSdd@users.noreply.github.com>
2019-01-23 22:28:03 +0600

    Add files via upload

---
+++ zadanie4ex1.js
@@ -0,0 +1,17 @@
+var text = document.getElementById('string-1')
+console.log(text.textContent);
+
+var text = document.getElementById('string-2')
+console.log(text.textContent);
+
+var text = document.getElementById('string-3')
+console.log(text.textContent);
+
+var text = document.getElementById('string-4')
+console.log(text.textContent);
+
+var text = document.getElementById('string-5')
+console.log(text.textContent);
+
+var text = document.getElementById('string-6')
+console.log(text.textContent);
