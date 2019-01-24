92c8349306f8347de0dcc3831e8ca926bf231b68
SaikalSdd <38737621+SaikalSdd@users.noreply.github.com>
2019-01-24 15:42:31 +0600

    Update 1-2-3zadachi.js.js

--- 1-2-3zadachi.js.js
+++ 1-2-3zadachi.js.js
@@ -1,9 +1,9 @@
-//Задача #1
-//for (var i = 0; i < 10; i++) {
-//    setTimeout(function() {
-//        console.log(i);
-//    }
-//};
+/*Задача #1
+for (var i = 0; i < 10; i++) {
+    setTimeout(function() {
+        console.log(i);
+    }
+*/};
 //Что выведет консоль и почему?
 Вывело: "Uncaught SyntaxError: missing ) after argument list"
 После добавления ")": "Uncaught SyntaxError: Unexpected token )"
@@ -14,19 +14,19 @@ setTimeout(функция, 10000);
 
 
 Задача #2
-//Write a JavaScript program to display the current day and time in the following format.  
-//Sample Output : 
-//Today is : Friday. 
-//Current time is : 4 PM : 50 : 22
+/*Write a JavaScript program to display the current day and time in the following format.  
+Sample Output : 
+Today is : Friday. 
+Current time is : 4 PM : 50 : 22*/
 var date = new Date();
 var dateNum = date.getDay();
 var dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 console.log("Today is : " + dayName[dateNum]);
 console.log("Current time is: " + date.getHours() + "PM :" + date.getMinutes() + " : " + date.getSeconds())
 
-//Write a JavaScript function that reverse a number. 
-//Example x = 32243;
-//Expected Output : 34223
+/*Write a JavaScript function that reverse a number. 
+Example x = 32243;
+Expected Output : 34223*/
 
 var num=prompt("Enter a number: ", "your num");
 function reversing(num){
@@ -35,8 +35,8 @@ function reversing(num){
 }
 reversing(num);
 
-//Write a JavaScript program to calculate the factorial of a number. In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. 
-//For example, 5! = 5 x 4 x 3 x 2 x 1 = 120 
+/*Write a JavaScript program to calculate the factorial of a number. In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. 
+For example, 5! = 5 x 4 x 3 x 2 x 1 = 120 */
 var num=prompt("Enter a number: ", "your num");
 function fact(num) { 
   if (num === 0){
@@ -58,11 +58,11 @@ else if(num1<num2){
 }
 alert(result+" is larger");
 
-//Write a simple JavaScript program to join all elements of the following array into a string. 
-//Sample array : myColor = ["Red", "Green", "White", "Black"];
-//Expected Output : 
-//"Red,Green,White,Black"
-//"Red+Green+White+Black"
+/*Write a simple JavaScript program to join all elements of the following array into a string. 
+Sample array : myColor = ["Red", "Green", "White", "Black"];
+Expected Output : 
+"Red,Green,White,Black"
+"Red+Green+White+Black"*/
 var myColor = ["Red", "Green", "White", "Black"];
 console.log(myColor.join(" "));
 
